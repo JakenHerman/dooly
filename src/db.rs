6e04b115a1a551189bf35805bd02c5949d6826e0
Jaken Herman <jakenherman7@gmail.com>
2024-10-14 17:47:45 -0500

    Added logging to dooly
    Resolves #7

--- src/db.rs
+++ src/db.rs
@@ -7,14 +7,14 @@ use diesel::r2d2::{self, Pool, ConnectionManager};
 
 use crate::schema::todos;
 
-#[derive(Queryable, Serialize, Deserialize)]
+#[derive(Queryable, Serialize, Deserialize, Debug)]
 pub struct TodoItem {
     pub id: i32,
     pub title: String,
     pub completed: bool,
 }
 
-#[derive(Insertable, Deserialize)]
+#[derive(Insertable, Deserialize, Debug)]
 #[diesel(table_name = todos)]
 pub struct NewTodoItem<'a> {
     pub title: &'a str,
@@ -25,6 +25,7 @@ pub fn establish_connection() -> Pool<ConnectionManager<SqliteConnection>> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    info!("Establishing database connection with {}", database_url);
     let manager = ConnectionManager::<SqliteConnection>::new(database_url.clone());
     r2d2::Pool::builder()
         .build(manager)
